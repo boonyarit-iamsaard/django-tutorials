e8a4a62a36fd3e3e56e21ddbdd9bef154924363a
boonyarit-iamsaard <boonyarit.iamsaard@gmail.com>
2020-05-20 18:13:14 +0700

    Just another commit

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        "author": "BoonyaritI",
+        "title": "Blog Post 1",
+        "content": "content 1",
+        "date_posted": "May 20, 2020",
+    },
+    {
+        "author": "BoonyaritI",
+        "title": "Blog Post 2",
+        "content": "content 2",
+        "date_posted": "May 20, 2020",
+    },
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>About Page</h1>")
+    return render(request, "blog/about.html")
